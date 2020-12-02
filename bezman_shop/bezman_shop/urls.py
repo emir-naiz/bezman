635895f49bd6104df8b07ec993e2dcfdbde93a99
emir <naizabekoff@mail.ru>
2020-12-02 12:47:15 +0600

    write new path 'accounts'

--- bezman_shop/bezman_shop/urls.py
+++ bezman_shop/bezman_shop/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('supershop.urls')),
+    path('accounts/', include('account.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
