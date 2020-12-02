f674e607fd5d9d53dae2dde22fe5a77fefbd0175
emir <naizabekoff@mail.ru>
2020-12-02 12:49:01 +0600

    add 3 def productList, orderList, orderCreate

--- bezman_shop/supershop/views.py
+++ bezman_shop/supershop/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from .forms import *
 # Create your views here.
 
-def productlist(request):
+def productList(request):
     products = Product.objects.all()
-    return render(request,'supershop/products.html')
+    context = {'products': products}
+    return render(request, 'supershop/products.html', context)
 
+def orderList(request):
+    orders = Order.objects.all()
+    context = {'orders': orders}
+    return render(request, 'supershop/order-list.html', context)
+
+
+def orderCreate(request, product_id):
+    product = Product.objects.get(id=product_id)
+    form = OrderForm(initial={'product': product})
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('orders')
+    context = {'form': form}
+    return render(request, 'supershop/order-create.html', context)
