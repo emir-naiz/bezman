f228a452ab02db8019cf907eb562f75216a963bb
emir <naizabekoff@mail.ru>
2020-12-02 12:47:53 +0600

    write all path

--- bezman_shop/supershop/urls.py
+++ bezman_shop/supershop/urls.py
@@ -2,7 +2,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from .views import *
 
 urlpatterns = [
-
+    path('products/', productList, name='products'),
+    path('orders/', orderList, name='orders'),
+    path('order-create/<int:product_id>/', orderCreate, name='order-create')
 ]
