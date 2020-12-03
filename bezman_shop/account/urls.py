bff9ac911a1961117c831e26da0790e2eacd9fdf
emir <naizabekoff@mail.ru>
2020-12-03 12:56:34 +0600

    add paths

--- bezman_shop/account/urls.py
+++ bezman_shop/account/urls.py
@@ -2,7 +2,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from .views import *
 
 urlpatterns = [
-
+    path('customers/', customerList, name='customers'),
+    path('customer/<int:customer_id>/', getCustomer, name='getcustomer'),
+    path('user-create/', createUser, name='user-create'),
 ]
