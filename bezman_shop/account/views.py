18032f05c75d8d8e978dd7b78accb6a46bd59d50
emir <naizabekoff@mail.ru>
2020-12-03 12:57:47 +0600

    add more def and import

--- bezman_shop/account/views.py
+++ bezman_shop/account/views.py
@@ -1,4 +1,28 @@
-from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from .models import *
 # Create your views here.
-from .forms import *
+
+
+def customerList(request):
+    customers = Customer.objects.all()
+    context = {'customers': customers}
+    return render(request, 'account/customers.html', context)
+
+def getCustomer(request, customer_id):
+    customer = Customer.objects.get(id=customer_id)
+    orders = customer.order_set.all()
+    context = {'customer': customer, 'orders': orders}
+    return render(request, 'account/getcustomer.html', context)
+
+def createUser(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('customers')
+    context = {'form': form}
+    return render(request, 'account/user-create.html', context)
+
+
