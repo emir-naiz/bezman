2af17061efbea5c2590e2d814580852690e5ea74
emir <naizabekoff@mail.ru>
2020-12-03 12:54:35 +0600

    create forms for account

---
+++ bezman_shop/account/forms.py
@@ -0,0 +1,9 @@
+# from django.contrib.auth.forms import UserCreationForm
+# from .models import *
+#
+# class CustomerForm(UserCreationForm):
+#
+#     class Meta:
+#         model = Customer
+#         fields = '__all__'
+#         exclude = ['status']
