0d20a5775fc1eece5bb6d303bae97ab7d604ab55
emir <naizabekoff@mail.ru>
2020-12-03 12:55:34 +0600

    add mail,image & class Meta

--- bezman_shop/account/models.py
+++ bezman_shop/account/models.py
@@ -5,7 +5,12 @@ class Customer(models.Model):
     full_name = models.CharField(max_length=70)
     address = models.CharField(max_length=50)
     phone = models.IntegerField()
+    mail = models.EmailField()
+    image = models.ImageField(blank=True)
 
     def __str__(self):
         return self.full_name
 
+    class Meta:
+        verbose_name="Клиент"
+        verbose_name_plural="Клиенты"
