e7ccf791bd14ec1501b01d129152f28052e96ab0
emir <naizabekoff@mail.ru>
2020-12-03 12:57:01 +0600

    nothing

--- bezman_shop/bezman_shop/urls.py
+++ bezman_shop/bezman_shop/urls.py
@@ -21,6 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('supershop.urls')),
-    path('accounts/', include('account.urls')),
+    path('account/', include('account.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
