871cac607b8e299aedeba03d68720c691d29b829
emir <naizabekoff@mail.ru>
2020-12-04 14:08:54 +0600

    write orders for Orders counting

--- bezman_shop/supershop/views.py
+++ bezman_shop/supershop/views.py
@@ -11,7 +11,13 @@ def productList(request):
 
 def orderList(request):
     orders = Order.objects.all()
-    context = {'orders': orders}
+    orders_count = orders.count()
+    orders_delivered = Order.objects.filter(status='Delivered').count()
+    orders_in_process = Order.objects.filter(status='In process').count()
+    orders_not_delivered = Order.objects.filter(status='Not delivered').count()
+
+    context = {'orders': orders, 'orders_count': orders_count, 'orders_delivered': orders_delivered,
+               'orders_in_process': orders_in_process, 'orders_not_delivered': orders_not_delivered}
     return render(request, 'supershop/order-list.html', context)
 
 
@@ -21,6 +27,17 @@ def orderCreate(request, product_id):
 
     if request.method == 'POST':
         form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('orders')
+    context = {'form': form}
+    return render(request, 'supershop/order-create.html', context)
+
+def orderUpdate(request, order_id):
+    order = Order.objects.get(id=order_id)
+    form = OrderForm(instance=order)
+    if request.method == 'POST':
+        form = OrderForm(request.POST, instance=order)
         if form.is_valid():
             form.save()
             return redirect('orders')
