90037de3cb3a44fca3f4f049082ae62ca8c92c66
emir <naizabekoff@mail.ru>
2020-12-04 14:04:24 +0600

    change class for change language

--- bezman_shop/account/apps.py
+++ bezman_shop/account/apps.py
@@ -1,5 +1,9 @@
 from django.apps import AppConfig
 
 
-class AccountConfig(AppConfig):
+# class AccountConfig(AppConfig):
+#     name = 'account'
+
+class FacesetConfig(AppConfig):
     name = 'account'
+    verbose_name = "Аккаунт"
