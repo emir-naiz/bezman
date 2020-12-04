3b61678228813453dbd40e29deae7c8275b3b2eb
emir <naizabekoff@mail.ru>
2020-12-04 14:04:37 +0600

    change class for change language

--- bezman_shop/supershop/apps.py
+++ bezman_shop/supershop/apps.py
@@ -1,5 +1,9 @@
 from django.apps import AppConfig
 
 
-class SupershopConfig(AppConfig):
+# class SupershopConfig(AppConfig):
+#     name = 'supershop'
+
+class FacesetConfig(AppConfig):
     name = 'supershop'
+    verbose_name = "Магазин"
