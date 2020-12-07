00e36892b3c1e00f43b2ca716d936f62b3175bcd
emir <naizabekoff@mail.ru>
2020-12-07 12:46:14 +0600

    write 2 def auth, logout_page

--- bezman_shop/account/views.py
+++ bezman_shop/account/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from .models import *
@@ -25,4 +26,18 @@ def createUser(request):
     context = {'form': form}
     return render(request, 'account/user-create.html', context)
 
+def auth(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('products')
+    context = {}
+    return render(request, 'account/login.html', context)
+
+def logout_page(request):
+    logout(request)
+    return redirect('login')
 
