d91c8abdce69381036c853a07f20c318dc1320f8
emir <naizabekoff@mail.ru>
2020-12-09 14:04:40 +0600

    add default for image

--- bezman_shop/account/models.py
+++ bezman_shop/account/models.py
@@ -6,7 +6,7 @@ class Customer(models.Model):
     address = models.CharField(max_length=50)
     phone = models.IntegerField()
     mail = models.EmailField()
-    image = models.ImageField(blank=True)
+    image = models.ImageField(blank='True', default='anonym.png')
 
     def __str__(self):
         return self.full_name
