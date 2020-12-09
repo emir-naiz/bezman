8271190bf6ac6ef5f2c7b72163142d45aabe9d8b
emir <naizabekoff@mail.ru>
2020-12-09 14:05:33 +0600

    add allowed_hosts [*]

--- bezman_shop/bezman_shop/settings.py
+++ bezman_shop/bezman_shop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'm$n-)=@w-nqia*4azh$zi*vswc6r=n@l203(2op@$jkew@kpzr'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
