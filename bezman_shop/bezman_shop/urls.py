504836a7007f00a5acbcf07fcc720968eb6a1fae
John Elder <john@codemy.com>
2020-12-10 14:16:06 +0600

    from supershop, add path(products)

--- bezman_shop/bezman_shop/urls.py
+++ bezman_shop/bezman_shop/urls.py
@@ -17,11 +17,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from supershop.views import productList
 
 urlpatterns = [
     path('jet/', include('jet.urls', 'jet')),  # Django JET URLS
     path('jet.dashboard',include('jet.dashboard.urls','jet-dashboard')), # Django JET dashboard URLS
     path('admin/', admin.site.urls),
+    path('', productList, name='products'),
     path('shop/', include('supershop.urls')),
     path('account/', include('account.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
