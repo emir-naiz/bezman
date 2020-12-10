20325c34d0585c50987a8ba45c9f217a7f62d9d4
John Elder <john@codemy.com>
2020-12-10 13:27:42 +0600

    add user in class Customer

--- bezman_shop/account/models.py
+++ bezman_shop/account/models.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
 class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=70)
     address = models.CharField(max_length=50)
     phone = models.IntegerField()
