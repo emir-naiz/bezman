29e84fb560cacb9828b7074a54fbc133731d4171
John Elder <john@codemy.com>
2020-12-11 12:33:41 +0600

    add 1 path for orderDelete

--- bezman_shop/supershop/urls.py
+++ bezman_shop/supershop/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('orders/', orderList, name='orders'),
     path('order-create/<int:product_id>/', orderCreate, name='order-create'),
     path('order-update/<int:order_id>/', orderUpdate, name='order-update'),
+    path('order-delete/<int:order_id>/', orderDelete, name='order-delete'),
 ]
