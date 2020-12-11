88099055388d3e4ff35fe8017b08c26856152931
John Elder <john@codemy.com>
2020-12-11 12:33:17 +0600

    add 4 path for pass

--- bezman_shop/account/urls.py
+++ bezman_shop/account/urls.py
@@ -3,8 +3,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from .views import *
+from django.contrib.auth.views import *
 
 urlpatterns = [
+    path('reset-password/', PasswordResetView.as_view(), name='reset-password'),
+    path('reset-password-done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm' ),
+    path('reset-password-complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('customers/', customerList, name='customers'),
     path('customer/<int:customer_id>/', getCustomer, name='getcustomer'),
     path('user-create/', createUser, name='user-create'),
