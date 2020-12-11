f02ba47cdae352d443662dad64597e5f960e0179
John Elder <john@codemy.com>
2020-12-11 12:32:58 +0600

    add jet theme, email host,port,use_tls,host_user,host_pass

--- bezman_shop/bezman_shop/settings.py
+++ bezman_shop/bezman_shop/settings.py
@@ -130,3 +130,12 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+JET_DEFAULT_THEME = 'green'
+JET_SIDE_MENU_COMPACT = True
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'bezman.shop@gmail.com'
+EMAIL_HOST_PASSWORD = 'pythonmorning123'
